e83f6a6467cb07ee487c53ca87ca93eed2a94b10
nurkhamidah <nurkhamidah.stat@gmail.com>
2024-06-04 21:29:41 +0700

    STREAM finish chatbot

--- stream.py
+++ stream.py
@@ -34,23 +34,70 @@ page = hc.nav_bar(
 )
 
 ## ------------------------------ TAB HOME ------------------------------
+if page == 'home':
+    st.header('Introduction to The Project')
+    
+    
+    
 
 ## ------------------------------ TAB ABOUT ------------------------------
-
+if page == 'about':
+    st.header('Introduction to The Team')
 
 ## ------------------------------ TAB PDRB ------------------------------
-
+if page == 'pdrb':
+    st.header('Produk Domestik Regional Bruto (PDRB) 34 Provinsi')
 
 ## ------------------------------ TAB EKSIM ------------------------------
-
+if page == 'eksim':
+    st.header('Alur Transaksi Ekspor-Impor antar 34 Provinsi')
 
 ## ------------------------------ TAB FLBL ------------------------------
-
+if page == 'flbl':
+    st.header('Sektor Unggulan 34 Provinsi berdasarkan Forward & Backward Linkage')
 
 ## ------------------------------ TAB SIMULATION ------------------------------
-
+if page == 'simul':
+    st.header('Simulasi Perhitungan PDRB berdasarkan Provinsi dan Industri')
 
 ## ------------------------------ TAB CLUSTERING ------------------------------
-
+if page == 'clust':
+    st.header('Analisis Klasterisasi 34 Provinsi di Indonesia dari Berbagai Sektor')
 
 ## ------------------------------ TAB CHATBOT ------------------------------
+if page == 'chat':
+    st.header('Chatbot IRIO Indonesia')
+    
+    api_key = OpenAI(api_key = st.secrets['OPENAI_API_KEY'])
+    @st.cache_resource(show_spinner=False)
+    def load_data():
+        with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+            reader = SimpleDirectoryReader(input_dir="./corpus", recursive=True)
+            docs = reader.load_data()
+            service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+            index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+            return index
+
+    index = load_data()
+    chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages: 
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input("Silakan input keyword tentang hasil analisis table IRIO..."):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+                st.markdown(prompt)
+            
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = chat_engine.chat(prompt)
+                st.write(response.response)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message) 
